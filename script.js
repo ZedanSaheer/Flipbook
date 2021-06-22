9abb67ba4abce3b42e8ffb3c7379606f4b65ecd5
Zedan Saheer <zed.saheer5@gmail.com>
2021-06-23 02:53:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const x = document.getElementById("page");
+const button = document.getElementById("fullscreen");
 
 $(function () {
     var $mybook = $('#mybook');
@@ -35,6 +35,68 @@ $(function () {
 
 });
 
+//sound on page swipe
 function pageSound(){
+    const x = document.getElementById("page")
     x.play();
+}
+
+//full screen toggle
+
+if (!Element.prototype.requestFullscreen) {
+	Element.prototype.requestFullscreen = Element.prototype.mozRequestFullscreen || Element.prototype.webkitRequestFullscreen || Element.prototype.msRequestFullscreen;
+}
+
+
+if (!document.exitFullscreen) {
+	document.exitFullscreen = document.mozExitFullscreen || document.webkitExitFullscreen || document.msExitFullscreen;
+}
+
+if (!document.fullscreenElement) {
+
+	Object.defineProperty(document, 'fullscreenElement', {
+		get: function() {
+			return document.mozFullScreenElement || document.msFullscreenElement || document.webkitFullscreenElement;
+		}
+	});
+
+	Object.defineProperty(document, 'fullscreenEnabled', {
+		get: function() {
+			return document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitFullscreenEnabled;
+		}
+	});
+}
+
+document.addEventListener('click', function (event) {
+
+	if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
+
+	if (document.fullscreenElement) {
+		document.exitFullscreen();
+        button.innerText="Toggle Fullscreen";
+	} else {
+		document.documentElement.requestFullscreen();
+        button.innerText="Exit Fullscreen";
+	}
+
+    
+}, false);
+
+//background changer
+
+var color;
+var defaultColor = "#0000ff";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+  color = document.querySelector("#color");
+  color.value = defaultColor;
+  color.addEventListener("input", updateFirst, false);
+}
+function updateFirst(event) {
+  var body = document.body;
+  if (body) {
+    body.style.backgroundColor = event.target.value;
+  }
 }
